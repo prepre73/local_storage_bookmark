8d5ec4ab60e4aa3e28449dda7e2dc9eda9333d53
prepre73 <35939658+prepre73@users.noreply.github.com>
2018-01-29 23:08:33 +0000

    initial commit for view
    Adding html to control the form input

---
+++ app/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Bookmark Local Storage V1</title>
+  <meta name="description" content="The HTML5 Herald">
+  <meta name="author" content="SitePoint">
+  <link rel="stylesheet" href="./style.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body>
+<div class="wrapper">
+    <div class="container container__form hero">
+        <div class="container__inner">
+            <h1>Enter Bookmark</h1>
+            <p>Add a bookmark by filling out the form below</p>
+            <form id="my__form">
+              <input type="text" placeholder="Website Name" id="site__name"/>
+              <input type="text" placeholder="Website Url" id="site__utl"/>
+              <button type="submit">submit</button>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+  <script src="./bookmark.js"></script>
+</body>
+</html>
